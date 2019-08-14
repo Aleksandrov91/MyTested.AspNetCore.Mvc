edbba412ea31c7830e19158600c8d5cd422760cb
Aleksandrov91 <a.aleksandrov91@gmail.com>
2019-08-14 20:36:57 +0300

    Add unit tests for account routing and controller.

--- samples/MusicStore/MusicStore.Test/Controllers/AccountControllerTest.cs
+++ samples/MusicStore/MusicStore.Test/Controllers/AccountControllerTest.cs
@@ -11,6 +11,9 @@
 
     public class AccountControllerTest
     {
+        private const string ValidUserId = "ValidUserId";
+        private const string ValidToken = "ValidToken";
+
         [Fact]
         public void AccountControllerShouldHaveAuthorizeFilter()
         {
@@ -185,6 +188,336 @@
                 .AndAlso()
                 .ShouldReturn()
                 .View(model);
+        }
+
+        [Fact]
+        public void GetVerifyCodeShouldHaveAllowAnonymosActionFilter()
+        {
+            MyMvc
+                .Controller<AccountController>()
+                .Calling(c => c.VerifyCode(With.No<string>(), With.No<bool>(), With.No<string>()))
+                .ShouldHave()
+                .ActionAttributes(attrs => attrs
+                        .AllowingAnonymousRequests());
+        }
+
+        [Fact]
+        public void GetVerifyCodeWithoutUserShouldReturnErrorView()
+        {
+            MyMvc
+                .Controller<AccountController>()
+                .Calling(c => c.VerifyCode(With.No<string>(), With.No<bool>(), With.No<string>()))
+                .ShouldReturn()
+                .View("Error");
+        }
+
+        [Fact]
+        public void GetVerifyCodeWithUserShouldReturnCorrectView()
+        {
+            var model = new VerifyCodeViewModel
+            {
+                Provider = "MyTest.com",
+                RememberMe = true,
+                ReturnUrl = "MyReturnUrl"
+            };
+
+            MyMvc
+                .Controller<AccountController>()
+                .Calling(c => c.VerifyCode(model.Provider, model.RememberMe, model.ReturnUrl))
+                .ShouldReturn()
+                .View(model);
+        }
+
+        [Fact]
+        public void PostVerifyCodeShouldHaveCorrectActionAttributes()
+        {
+            MyMvc
+                .Controller<AccountController>()
+                .Calling(c => c.VerifyCode(With.Default<VerifyCodeViewModel>()))
+                .ShouldHave()
+                .ActionAttributes(attrs => attrs
+                    .RestrictingForHttpMethod(HttpMethod.Post)
+                    .AllowingAnonymousRequests()
+                    .ValidatingAntiForgeryToken());
+        }
+
+        [Fact]
+        public void PostVerifyCodeWithInvalidModelShouldContainErrorsAndReturnView()
+        {
+            MyMvc
+                .Controller<AccountController>()
+                .Calling(c => c.VerifyCode(With.Default<VerifyCodeViewModel>()))
+                .ShouldHave()
+                .ModelState(modelstate => modelstate
+                    .For<VerifyCodeViewModel>()
+                    .ContainingErrorFor(m => m.Code)
+                    .ContainingErrorFor(m => m.Provider))
+                .AndAlso()
+                .ShouldReturn()
+                .View(With.No<VerifyCodeViewModel>());
+        }
+
+        [Fact]
+        public void PostVerifyCodeWithoutCodeShouldContainModelStateErrorAndReturnView()
+        {
+            var model = new VerifyCodeViewModel
+            {
+                Provider = "MyProvider",
+                ReturnUrl = "MyReturnUrl"
+            };
+
+            MyMvc
+                .Controller<AccountController>()
+                .Calling(c => c.VerifyCode(model))
+                .ShouldHave()
+                .ModelState(modelstate => modelstate
+                    .For<VerifyCodeViewModel>()
+                    .ContainingErrorFor(m => m.Code))
+                .AndAlso()
+                .ShouldReturn()
+                .View(model);
+        }
+
+        [Fact]
+        public void GetRegisterShouldHaveAllowAnonymosFilter()
+        {
+            MyMvc
+                .Controller<AccountController>()
+                .Calling(c => c.Register())
+                .ShouldHave()
+                .ActionAttributes(attrs => attrs
+                    .AllowingAnonymousRequests());
+        }
+
+        [Fact]
+        public void GetRegisterShouldReturnView()
+        {
+            MyMvc
+                .Controller<AccountController>()
+                .Calling(c => c.Register())
+                .ShouldReturn()
+                .View();
+        }
+
+        [Fact]
+        public void PostRegisterShouldHaveCorrectActionFilters()
+        {
+            MyMvc
+                .Controller<AccountController>()
+                .Calling(c => c.Register(With.Default<RegisterViewModel>()))
+                .ShouldHave()
+                .ActionAttributes(attrs => attrs
+                    .RestrictingForHttpMethod(HttpMethod.Post)
+                    .AllowingAnonymousRequests()
+                    .ValidatingAntiForgeryToken());
+        }
+
+        [Fact]
+        public void PostRegisterShouldReturnDefaultViewWithInvalidModel()
+        {
+            var model = new RegisterViewModel
+            {
+                Email = "Invalid@invalid.com",
+                Password = "Invalid",
+                ConfirmPassword = "Not Valid"
+            };
+
+            MyMvc
+                .Controller<AccountController>()
+                .Calling(c => c.Register(model))
+                .ShouldHave()
+                .ModelState(modelstate => modelstate
+                    .For<RegisterViewModel>()
+                    .ContainingErrorFor(m => m.ConfirmPassword))
+                .AndAlso()
+                .ShouldReturn()
+                .View(model);
+
+        }
+
+        [Fact]
+        public void PostRegisterWithValidModelShouldReturnRedirectToAction()
+        {
+            var model = new RegisterViewModel
+            {
+                Email = "Valid@valid.com",
+                Password = "ValidPassword#123",
+                ConfirmPassword = "ValidPassword#123"
+            };
+
+            MyMvc
+                .Controller<AccountController>()
+                .Calling(c => c.Register(model))
+                .ShouldReturn()
+                .Redirect(redirect => redirect
+                    .To<HomeController>(c => c.Index(
+                        With.No<MusicStoreContext>(),
+                        With.No<IMemoryCache>())));
+        }
+
+        [Fact]
+        public void GetConfirmEmailShouldHaveAllowAnonymosFilter()
+        {
+            MyMvc
+                .Controller<AccountController>()
+                .Calling(c => c.ConfirmEmail(With.No<string>(), With.No<string>()))
+                .ShouldHave()
+                .ActionAttributes(attrs => attrs
+                    .AllowingAnonymousRequests());
+        }
+
+        [Fact]
+        public void GetConfirmEmailWithInvalidParametersShouldReturnErrorView()
+        {
+            MyMvc
+                .Controller<AccountController>()
+                .Calling(c => c.ConfirmEmail(With.No<string>(), With.No<string>()))
+                .ShouldReturn()
+                .View("Error");
+        }
+
+        [Fact]
+        public void GetConfirmEmailWithValidUserIdAndNoCodeShouldReturnErrorView()
+        {
+            MyMvc
+                .Controller<AccountController>()
+                .Calling(c => c.ConfirmEmail(ValidUserId, With.No<string>()))
+                .ShouldReturn()
+                .View("Error");
+        }
+
+        [Fact]
+        public void GetConfirmEmailWithCodeANdNoUserShouldReturnErrorView()
+        {
+            MyMvc
+                .Controller<AccountController>()
+                .Calling(c => c.ConfirmEmail(With.No<string>(), ValidToken))
+                .ShouldReturn()
+                .View("Error");
+        }
+
+        [Fact]
+        public void GetConfirmEmailWithInvalidUserAndValidTokenShouldReturnErrorView()
+        {
+            var invalidUser = "InvalidUser";
+
+            MyMvc
+                .Controller<AccountController>()
+                .Calling(c => c.ConfirmEmail(invalidUser, ValidToken))
+                .ShouldReturn()
+                .View("Error");
+        }
+
+        [Fact]
+        public void GetConfirmEmailWithInvalidCodeShouldReturnView()
+        {
+            var invalidCode = "InvalidCode";
+
+            MyMvc
+                .Controller<AccountController>()
+                .Calling(c => c.ConfirmEmail(ValidUserId, invalidCode))
+                .ShouldReturn()
+                .View("Error");
+        }
+
+        [Fact]
+        public void GetConfirmEmailWithValidParametersShouldReturnView()
+        {
+            MyMvc
+                .Controller<AccountController>()
+                .Calling(c => c.ConfirmEmail(ValidUserId, ValidToken))
+                .ShouldReturn()
+                .View("ConfirmEmail");
+        }
+
+        [Fact]
+        public void GetForgotPasswordShouldHaveAllowAnonymosFilter()
+        {
+            MyMvc
+                .Controller<AccountController>()
+                .Calling(c => c.ForgotPassword())
+                .ShouldHave()
+                .ActionAttributes(attrs => attrs
+                    .AllowingAnonymousRequests());
+        }
+
+        [Fact]
+        public void GetForgotPasswordShouldReturnView()
+        {
+            MyMvc
+                .Controller<AccountController>()
+                .Calling(c => c.ForgotPassword())
+                .ShouldReturn()
+                .View();
+        }
+
+        [Fact]
+        public void PostForgotPasswordShouldHaveCorrectActionAttributes()
+        {
+            MyMvc
+                .Controller<AccountController>()
+                .Calling(c => c.ForgotPassword(With.Default<ForgotPasswordViewModel>()))
+                .ShouldHave()
+                .ActionAttributes(attrs => attrs
+                    .RestrictingForHttpMethods(HttpMethod.Post)
+                    .AllowingAnonymousRequests()
+                    .ValidatingAntiForgeryToken());
+        }
+
+        [Fact]
+        public void PostLogOffShouldHaveCorrectActionAttributes()
+        {
+            MyMvc
+                .Controller<AccountController>()
+                .Calling(c => c.LogOff())
+                .ShouldHave()
+                .ActionAttributes(attrs => attrs
+                    .RestrictingForHttpMethods(HttpMethod.Post)
+                    .ValidatingAntiForgeryToken());
+        }
+
+        [Fact]
+        public void PostLogOffShouldRedirectToHome()
+        {
+            MyMvc
+                .Controller<AccountController>()
+                .WithUser("Administrator@test.com")
+                .Calling(c => c.LogOff())
+                .ShouldReturn()
+                .RedirectToAction("Index");
+        }
+
+        [Fact]
+        public void PostLogOffShouldClearUserFromContext()
+        {
+            //TODO
+        }
+
+        [Fact]
+        public void PostLogOffWithoutAuthorizedUserShouldReturnError()
+        {
+            //TODO
+        }
+
+        [Fact]
+        public void GetExternalLoginFailureHaveAllowAnonymosActionAttribute()
+        {
+            MyMvc
+                .Controller<AccountController>()
+                .Calling(c => c.ExternalLoginFailure())
+                .ShouldHave()
+                .ActionAttributes(attrs => attrs
+                    .AllowingAnonymousRequests());
+        }
+
+        [Fact]
+        public void GetExternalLoginFaiulreShouldReturnView()
+        {
+            MyMvc
+                .Controller<AccountController>()
+                .Calling(c => c.ExternalLoginFailure())
+                .ShouldReturn()
+                .View();
         }
     }
 }
---
+++ samples/MusicStore/MusicStore.Test/Routing/AccountRouteTest.cs
@@ -0,0 +1,172 @@
+﻿namespace MusicStore.Test.Routing
+{
+    using MusicStore.Controllers;
+    using MusicStore.Models;
+    using MyTested.AspNetCore.Mvc;
+    using Xunit;
+
+    public class AccountRouteTest
+    {
+        [Fact]
+        public void GetLoginShouldBeRoutedCorrectly()
+        {
+            MyRouting
+                .Configuration()
+                .ShouldMap("/Account/Login")
+                .To<AccountController>(c => c.Login(With.No<string>()));
+        }
+
+        [Theory]
+        [InlineData("ValidUser@user.com", "1ValidPassword", true, "test.com")]
+        [InlineData("ValidUser@user.com", "1ValidPassword", true, null)]
+        public void PostLoginShouldBeRoutedCorrectly(string email, string password, bool rememberMe, string returnUrl)
+        {
+            MyRouting
+                .Configuration()
+                .ShouldMap(request => request
+                    .WithMethod(HttpMethod.Post)
+                    .WithLocation("/Account/Login")
+                    .WithFormFields(new
+                    {
+                        Email = email,
+                        Password = password,
+                        RememberMe = rememberMe.ToString(),
+                        ReturnUrl = returnUrl
+                    })
+                    .WithAntiForgeryToken())
+                .To<AccountController>(c => c.Login(new LoginViewModel
+                {
+                    Email = email,
+                    Password = password,
+                    RememberMe = rememberMe
+                }, returnUrl))
+                .AndAlso()
+                .ToValidModelState();
+        }
+
+        [Fact]
+        public void GetRegisterShouldBeRoutedCorrectly()
+        {
+            MyRouting
+                .Configuration()
+                .ShouldMap("/Account/Register")
+                .To<AccountController>(c => c.Register());
+        }
+
+        [Theory]
+        [InlineData("ValidUser@user.com", "1ValidPassword", "1ValidPassword")]
+        public void PostRegisterShouldBeRoutedCorrectly(string email, string password, string confirmPassword)
+        { 
+            MyRouting
+                .Configuration()
+                .ShouldMap(request => request
+                    .WithMethod(HttpMethod.Post)
+                    .WithLocation("/Account/Register")
+                    .WithFormFields(new
+                    {
+                        Email = email,
+                        Password = password,
+                        ConfirmPassword = confirmPassword
+                    })
+                    .WithAntiForgeryToken())
+                .To<AccountController>(c => c.Register(new RegisterViewModel
+                {
+                    Email = email,
+                    Password = password,
+                    ConfirmPassword = confirmPassword
+                }))
+                .AndAlso()
+                .ToValidModelState();
+        }
+
+        [Fact]
+        public void GetVerifyCodeShouldBeRoutedCorrectly()
+        {
+            MyRouting
+                .Configuration()
+                .ShouldMap("/Account/VerifyCode")
+                .To<AccountController>(c => c.VerifyCode(
+                    With.No<string>(),
+                    With.No<bool>(),
+                    With.No<string>()));
+        }
+
+        [Fact]
+        public void GetConfirmEmailShouldBeRoutedCorrectly()
+        {
+            MyRouting
+                .Configuration()
+                .ShouldMap("/Account/ConfirmEmail")
+                .To<AccountController>(c => c.ConfirmEmail(
+                    With.No<string>(),
+                    With.No<string>()));
+        }
+
+        [Fact]
+        public void GetForgotPasswordShouldBeRoutedCorrectly()
+        {
+            MyRouting
+                .Configuration()
+                .ShouldMap("/Account/ForgotPassword")
+                .To<AccountController>(c => c.ForgotPassword());
+        }
+
+        [Fact]
+        public void GetForgotPasswordConfirmationShouldBeRoutedCorrectly()
+        {
+            MyRouting
+                .Configuration()
+                .ShouldMap("/Account/ForgotPasswordConfirmation")
+                .To<AccountController>(c => c.ForgotPasswordConfirmation());
+        }
+
+        [Fact]
+        public void GetResetPasswordShouldBeRoutedCorrectly()
+        {
+            MyRouting
+                .Configuration()
+                .ShouldMap("/Account/ResetPassword")
+                .To<AccountController>(c => c.ResetPassword(With.No<string>()));
+        }
+
+        [Fact]
+        public void GetResetPasswordConfirmationShouldBeRoutedCorrectly()
+        {
+            MyRouting
+                .Configuration()
+                .ShouldMap("/Account/ResetPasswordConfirmation")
+                .To<AccountController>(c => c.ResetPasswordConfirmation());
+        }
+
+        [Fact]
+        public void GetSendCodeShouldBeRoutedCorrectly()
+        {
+            MyRouting
+                .Configuration()
+                .ShouldMap("/Account/SendCode")
+                .To<AccountController>(c => c.SendCode(
+                    With.No<bool>(),
+                    With.No<string>()));
+        }
+
+        [Fact]
+        public void GetExternalLoginCallbackShouldBeRoutedCorrectly()
+        {
+            MyRouting
+                .Configuration()
+                .ShouldMap("/Account/ExternalLoginCallback")
+                .To<AccountController>(c => c.ExternalLoginCallback(With.No<string>()));
+        }
+
+        [Fact]
+        public void GetExternalLoginFailureShouldBeRoutedCorrectly()
+        {
+            MyRouting
+                .Configuration()
+                .ShouldMap("/Account/ExternalLoginFailure")
+                .To<AccountController>(c => c.ExternalLoginFailure());
+        }
+
+        //TODO: Test all post methods
+    }
+}
