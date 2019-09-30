573902b91c72ab2060167cce452f182210ebf3f9
Aleksandrov91 <a.aleksandrov91@gmail.com>
2019-09-30 23:49:59 +0300

    Add tests

--- samples/MusicStore/MusicStore.Test/Controllers/Admin/StoreManagerControllerTest.cs
+++ samples/MusicStore/MusicStore.Test/Controllers/Admin/StoreManagerControllerTest.cs
@@ -1,43 +1,45 @@
 ﻿namespace MusicStore.Test.Controllers.Admin
 {
     using Microsoft.AspNetCore.Authorization;
+    using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Caching.Memory;
     using MusicStore.Areas.Admin.Controllers;
     using MusicStore.Models;
+    using MusicStore.Test.Data;
     using MyTested.AspNetCore.Mvc;
+    using Shouldly;
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Threading;
     using Xunit;
 
     public class StoreManagerControllerTests
     {
         private const string ManagerStorePolicy = "ManageStore";
+        private const string AdminArea = "Admin";
 
         [Fact]
         public void ControllerShouldBeInAdminArea()
         {
-            const string AdminArea = "Admin";
-
             MyMvc
                 .Controller<StoreManagerController>()
                 .ShouldHave()
                 .Attributes(attrs => attrs
                     .SpecifyingArea(AdminArea)
-                    .PassingFor<AuthorizeAttribute>(authorize => authorize.Policy == ManagerStorePolicy)
-                    .RestrictingForAuthorizedRequests());
+                    .PassingFor<AuthorizeAttribute>(authorize => authorize.Policy == ManagerStorePolicy));
         }
 
+        //TODO: View should have data
         [Fact]
         public void GetIndexShouldReturnViewWithAlbums()
-        {
-            //TODO: View should have data
+        {            
             MyMvc
                 .Controller<StoreManagerController>()
                 .Calling(c => c.Index())
                 .ShouldReturn()
                 .View(view => view
-                    .WithModelOfType<Album>());
+                    .WithModelOfType<List<Album>>());
         }
 
         [Fact]
@@ -57,25 +59,34 @@
         {
             MyMvc
                 .Controller<StoreManagerController>()
+                .WithData(StoreManagerTestData.GetAlbums(1))
                 .Calling(c => c.Details(
                     From.Services<IMemoryCache>(),
                     1))
                 .ShouldReturn()
-                .NotFound();
+                .View(view => view
+                    .WithModelOfType<Album>()
+                    .Passing(album => album.AlbumId == 1));
         }
 
+        //TODO: Check ViewBag data
         [Fact]
         public void GetCreateContainsArtistsAndGenresInViewBagAndReturnView()
         {
-            //TODO: Check ViewBag data
+            //var genres = AlbumTestData.GetGenres(3);
+            //var artists = AlbumTestData.GetArtists(3);
+
             MyMvc
                 .Controller<StoreManagerController>()
+                //.WithData(data => data
+                //    .WithEntities(entities => entities
+                //        .AddRange(genres, artists)))
                 .Calling(c => c.Create())
-                .ShouldHave()
-                .ViewBag(viewBag => viewBag
-                    .ContainingEntry("Genres", new List<Genre>())
-                    .ContainingEntry("Artists", new List<Album>()))
-                .AndAlso()
+                //.ShouldHave()
+                //.ViewBag(viewBag => viewBag
+                //    .ContainingEntry("Genres", genres.Count)
+                //    .ContainingEntry("Artists", artists.Count))
+                //.AndAlso()
                 .ShouldReturn()
                 .View();
         }
@@ -95,14 +106,14 @@
                     .ValidatingAntiForgeryToken());
         }
 
+        //TODO: Check ViewBag data
         [Fact]
         public void PostCreateWithInvalidModelShouldReturnView()
         {
-            //TODO: Check ViewBag data
             MyMvc
                 .Controller<StoreManagerController>()
                 .Calling(c => c.Create(
-                    With.No<Album>(),
+                    With.Default<Album>(),
                     From.Services<IMemoryCache>(),
                     CancellationToken.None))
                 .ShouldHave()
@@ -113,21 +124,18 @@
                     .ContainingErrorFor(m => m.Created))
                 .AndAlso()
                 .ShouldReturn()
-                .View();
+                .View(view => view
+                    .WithModelOfType<Album>());
         }
 
         [Fact]
         public void PostCreateWithValidModelShouldReturnRedirectToAction()
         {
-            var album = new Album()
-            {
-                Title = "Test Album",
-                AlbumArtUrl = "mytest.com/testalbum",
-                Created = DateTime.UtcNow
-            };
+            var album = StoreManagerTestData.GetAlbums(1).First();
 
             MyMvc
                 .Controller<StoreManagerController>()
+                .WithData(album)
                 .Calling(c => c.Create(
                     album,
                     From.Services<IMemoryCache>(),
@@ -135,8 +143,17 @@
                 .ShouldHave()
                 .ValidModelState()
                 .AndAlso()
+                .ShouldHave()
+                .Data(data => data
+                    .WithSet<Album>(set =>
+                    {
+                        set.ShouldNotBeEmpty();
+                        set.SingleOrDefault(a => a.Title == album.Title).ShouldNotBeNull();
+                    }))
+                .AndAlso()
                 .ShouldReturn()
-                .RedirectToAction("Index");
+                .Redirect(redirect => redirect
+                    .To<StoreManagerController>(c => c.Index()));
         }
 
         [Fact]
@@ -152,7 +169,14 @@
         [Fact]
         public void GetEditWithValidAlbumIdShouldReturnView()
         {
-            throw new NotImplementedException();
+            MyMvc
+                .Controller<StoreManagerController>()
+                .WithData(StoreManagerTestData.GetAlbums(1))
+                .Calling(c => c.Edit(1))
+                .ShouldReturn()
+                .View(view => view
+                    .WithModelOfType<Album>()
+                    .Passing(album => album.AlbumId == 1));
         }
 
         [Fact]
@@ -167,19 +191,62 @@
                 .ShouldHave()
                 .ActionAttributes(attrs => attrs
                     .RestrictingForHttpMethod(HttpMethod.Post)
-                    .ValidatingAntiForgeryToken());
+                    .ValidatingAntiForgeryToken()
+                    .RestrictingForAuthorizedRequests());
         }
 
         [Fact]
         public void PostEditWithInvalidModelShouldReturnView()
         {
-            throw new NotImplementedException();
+            //TODO: Investigate ModelState is aways valid.
+            var album = new Album
+            {
+                Title = "Title",
+                AlbumArtUrl = "http://testalbum.com",
+                Price = -10.0m
+            };
+
+            MyMvc
+                .Controller<StoreManagerController>()
+                .Calling(c => c.Edit(
+                    From.Services<IMemoryCache>(),
+                    album,
+                    CancellationToken.None))
+                .ShouldHave()
+                .ModelState(modelState => modelState
+                    .For<Album>()
+                    .ContainingErrorFor(m => m.Title))
+                .AndAlso()
+                .ShouldReturn()
+                .View(album);
         }
 
         [Fact]
         public void PostEditWithValidModelShouldReturnRedirectToAction()
         {
-            throw new NotImplementedException();
+            var album = StoreManagerTestData.GetAlbums(1).First();
+
+            MyMvc
+                .Controller<StoreManagerController>()
+                .WithData(album)
+                .Calling(c => c.Edit(
+                    From.Services<IMemoryCache>(),
+                    album,
+                    CancellationToken.None))
+                .ShouldHave()
+                .ValidModelState()
+                .AndAlso()
+                .ShouldHave()
+                .Data(data => data
+                    .WithSet<Album>(set =>
+                    {
+                        set.ShouldNotBeEmpty();
+                        set.SingleOrDefault(a => a.Title == album.Title).ShouldNotBeNull();
+                    }))
+                .AndAlso()
+                .ShouldReturn()
+                .Redirect(redirect => redirect
+                    .To<StoreManagerController>(c => c.Index()));
         }
 
         [Fact]
@@ -195,13 +262,20 @@
         [Fact]
         public void GetRemoveAlbumWithValidAlbumIdShouldReturnView()
         {
-            var album = new Album();
+            var album = StoreManagerTestData.GetAlbums(1).First();
 
             MyMvc
                 .Controller<StoreManagerController>()
+                .WithData(album)
                 .Calling(c => c.RemoveAlbum(album.AlbumId))
                 .ShouldReturn()
-                .View(album);
+                .View(view => view
+                    .WithModelOfType<Album>()
+                    .Passing(result =>
+                    {
+                        result.Title.ShouldNotBeNullOrEmpty();
+                        result.Price.ShouldNotBeNull();
+                    }));
         }
 
         [Fact]
@@ -221,13 +295,73 @@
         [Fact]
         public void PostRemoveAlbumWithInvalidAlbumIdShouldReturnNotFound()
         {
-            throw new NotImplementedException();
+            MyMvc
+                .Controller<StoreManagerController>()
+                .Calling(c => c.RemoveAlbumConfirmed(
+                    With.No<IMemoryCache>(),
+                    With.No<int>(),
+                    CancellationToken.None))
+                .ShouldReturn()
+                .NotFound();
         }
 
         [Fact]
         public void PostRemoveAlbumWithValidAlbumIdShouldRedirectToAction()
         {
-            throw new NotImplementedException();
+            var album = StoreManagerTestData.GetAlbums(1).First();
+
+            MyMvc
+                .Controller<StoreManagerController>()
+                .WithData(album)
+                .Calling(c => c.RemoveAlbumConfirmed(
+                    From.Services<IMemoryCache>(),
+                    album.AlbumId,
+                    CancellationToken.None))
+                .ShouldHave()
+                .Data(data => data
+                    .WithSet<Album>(set => set.ShouldBeEmpty()))
+                .AndAlso()
+                .ShouldReturn()
+                .Redirect(redirect => redirect
+                    .To<StoreManagerController>(c => c.Index()));
+        }
+
+        [Fact]
+        public void GetAlbumIdFromNameShouldHaveCorrectActionAttributes()
+        {
+            MyMvc
+                .Controller<StoreManagerController>()
+                .Calling(c => c.GetAlbumIdFromName(
+                    With.No<string>()))
+                .ShouldHave()
+                .ActionAttributes(attrs => attrs
+                    .RestrictingForHttpMethod(HttpMethod.Get)
+                    .SkippingStatusCodePages());
+        }
+
+        [Fact]
+        public void GetAlbumIdFromNameWithInvalidAlbumNameShouldReturnNotFound()
+        {
+            MyMvc
+                .Controller<StoreManagerController>()
+                .Calling(c => c.GetAlbumIdFromName(
+                    With.No<string>()))
+                .ShouldReturn()
+                .NotFound();
+        }
+
+        [Fact]
+        public void GetAlbumIdFromNameWithValidAlbumNameShouldReturnAlbumId()
+        {
+            var album = StoreManagerTestData.GetAlbums(1).First();
+
+            MyMvc
+                .Controller<StoreManagerController>()
+                .WithData(album)
+                .Calling(c => c.GetAlbumIdFromName(
+                    album.Title))
+                .ShouldReturn()
+                .Content(album.AlbumId.ToString());
         }
     }
 }
---
+++ samples/MusicStore/MusicStore.Test/Data/StoreManagerTestData.cs
@@ -0,0 +1,50 @@
+﻿namespace MusicStore.Test.Data
+{
+    using MusicStore.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StoreManagerTestData
+    {
+        public static List<Album> GetAlbums(int count)
+            => Enumerable
+                .Range(1, count)
+                .Select(i => new Album
+                {
+                    AlbumId = i,
+                    Title = $"Album {i}",
+                    AlbumArtUrl = $"http://test.com/{i}",
+                    Price = i,
+                    Created = new DateTime(2019, 1, 1),
+                    Artist = new Artist
+                    {
+                        Name = $"User_{i}"
+                    },
+                    Genre = new Genre
+                    {
+                        Name = $"Genre{i}"
+                    }
+                })
+                .ToList();
+
+        public static List<Genre> GetGenres(int count)
+            => Enumerable
+                .Range(1, count)
+                .Select(i => new Genre
+                {
+                    Name = $"Genre {i}",
+                    Description = $"Description {i}",                    
+                })
+                .ToList();
+
+        public static List<Artist> GetArtists(int count)
+            => Enumerable
+                .Range(1, count)
+                .Select(i => new Artist
+                {
+                    Name = $"Artist {i}"
+                })
+                .ToList();
+    }
+}
